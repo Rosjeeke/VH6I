5dfcf62f873a6b6f8a4da3083a57fa1b17c51be7
Rogier Welten <rogier_w@live.nl>
2013-12-03 11:18:41 +0100

    Transform verplaatst

---
+++ ECTL2/src/ectl/Procedures/Transform.java
@@ -0,0 +1,29 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ectl.Procedures;
+
+import ectl.Klant;
+import java.util.ArrayList;
+
+/**
+ *
+ * @author Rogier
+ */
+public class Transform {
+    
+    ArrayList<Klant> klanten;
+    
+    public Transform(){
+        klanten = new ArrayList<Klant>();
+    }
+    
+    public void Transform(){
+    
+        for(Klant k: klanten){
+            int percentage = k.getPercentage();
+            if()
+        }
+    }
+}
