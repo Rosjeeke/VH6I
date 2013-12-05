01c7c930fdfa2fd6d4c11f7122170aade540948c
IvoHefti <ivo_dude@hotmail.com>
2013-12-05 10:14:50 +0100

    Transform van de namen is bijna af

--- ECTL2/src/ectl/Procedures/Transform.java
+++ ECTL2/src/ectl/Procedures/Transform.java
@@ -8,8 +8,6 @@ import ectl.Klant;
 import java.math.BigDecimal;
 import java.math.RoundingMode;
 import java.util.ArrayList;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
 
 /**
  *
@@ -20,8 +18,8 @@ public class Transform {
     ArrayList<Klant> klanten;
 
     public Transform() {
-        klanten = new ArrayList<Klant>();
-        klanten.add(new Klant(1, "Jeroen v.d. Doorn", "0162683112", "0627232007", "Made", "PO", new BigDecimal("0.15")));
+        klanten = new ArrayList<>();
+        klanten.add(new Klant(1, "R.Welten", "0162683112", "0627232007", "Made", "PO", new BigDecimal("0.15")));
     }
 
     public void Transform() {
@@ -55,50 +53,49 @@ public class Transform {
             }
 
             String naam = k.getKlantNaam();
-            if (naam.contains(".")){ 
-                String naam1 = naam.substring(0, naam.indexOf('.'));
-                String naam2 = naam.substring(naam.indexOf('.') + 1);
-                    if (naam1.length() == 1 && !k.getKlantNaam().contains(" en ")){            
-                        System.out.println(naam1);
-                        System.out.println(naam2);
+            if (naam.contains(".")) {
+                if (naam.contains(" v.d.")) {
+                    String naamNieuw = k.getKlantNaam().replace(" v.d. ", " v/d ");
+                    k.setKlantNaam(naamNieuw);
+                } else {
+                    String naam1 = naam.substring(0, naam.indexOf('.'));
+                    String naam2 = naam.substring(naam.indexOf('.') + 1);
+                    if (naam1.length() == 1 && !k.getKlantNaam().contains(" en ")) {
                         k.setKlantNaam(naam1 + "." + naam2);
-                        System.out.println("eerste " + k.getKlantNaam());
+                        System.out.println(k.getKlantNaam());
                     }
+                }
             }
-            if (k.getKlantNaam().contains(" en ")) {
-                String[] delen = k.getKlantNaam().split(" en ");
-                String naam3 = delen[0];
-                String naam4 = delen[1];
-                k.setKlantNaam(naam3 + " - " + naam4);
-                System.out.println("tweede " + k.getKlantNaam());
-            }
-//            if (){
-//                
-//            }
             
-            char ch = k.getKlantNaam().charAt(2);
+            if (k.getKlantNaam().contains(",")){
+                String naam1 = naam.substring(0, naam.indexOf(','));
+                String naam2 = naam.substring(naam.indexOf(',') + 1);
+                k.setKlantNaam(naam2.replace(" ", "") + " " + naam1);
+                System.out.println(k.getKlantNaam());
+            }
+            
+            if (k.getKlantNaam().contains(" en ")) {
+                String naamNieuw = k.getKlantNaam().replace(" en ", " - ");
+                k.setKlantNaam(naamNieuw);
+                System.out.println(k.getKlantNaam());
+            }
+
+            String[] tussenvoegsels = new String[]{" van de ", " van der ", " Van De ", " Van Der ", " van den ", " Van Den "};
+            for (String woord : tussenvoegsels) {
+                if (k.getKlantNaam().indexOf(woord) > 0) {
+                    String naamNieuw = k.getKlantNaam().replace(woord, " v/d ");
+                    k.setKlantNaam(naamNieuw);
+                }
+            }
+
+            char ch = k.getKlantNaam().charAt(1);
             if (Character.isLetter(ch)) {
-                String naam1 = naam.substring(0, naam.indexOf(' '));
-                String naam2 = naam.substring(naam.indexOf(' ') + 1);
+                String naam1 = k.getKlantNaam().substring(0, k.getKlantNaam().indexOf(' '));
+                String naam2 = k.getKlantNaam().substring(k.getKlantNaam().indexOf(' ') + 1);
                 String naam3 = naam1.substring(0, 1);
-                k.setKlantNaam(naam3+ ". " + naam2);
-                System.out.println("derde " + k.getKlantNaam());
-            }
-            else{
-                System.out.println("Faal");
-            }
-
-
-
-
-//          Pattern pattern = Pattern.compile("\\[A-Z]{1}.{1}\\p{IsWhite_Space}{1}[A-Za-z]{1,50}");
-//          Matcher matcher = pattern.matcher(naam);
-//          
-//          if(matcher.matches()){
-//              System.out.println("Goed");
-//          }
-//          else{System.out.println("fout");}
-//        }
+                k.setKlantNaam(naam3 + ". " + naam2);
+                System.out.println(k.getKlantNaam());
+            } 
         }
     }
 }
